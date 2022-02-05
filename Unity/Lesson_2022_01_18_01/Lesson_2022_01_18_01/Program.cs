2c22dca7ca2a9d0f73a8fa458cec190218203a40
Chadolbaegi128 <duswns12893@gmail.com>
2022-02-05 20:25:46 +0900

    Unity Lesson Update-2

---
+++ Unity/Lesson_2022_01_18_01/Lesson_2022_01_18_01/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld_CSharp
+
+{
+   class Program
+  {
+	
+		static string PlusString(string A, string B)
+		{
+			return A + B;
+		}
+	}
+}
---
+++ Unity/Lesson_2022_01_18_02/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_2022_01_18_02
+{    
+
+    class Program
+    {
+
+        class Class2
+        {
+            public string i = "클래스";
+            public void method()
+            {
+                Console.WriteLine("ClassA");
+            }
+        }
+       
+        static string PlusString(string A, string B)
+        {
+            return A + B;
+        }
+
+        static void Main(string[] args)
+        {
+            Class2 class2 = new Class2();
+            Console.WriteLine(class2.i);
+            Console.Write("메소드 출력: ");
+            class2.method();
+            string result = PlusString("안녕", "하세요");
+            Console.WriteLine(result);
+        }
+        
+
+    }
+}
