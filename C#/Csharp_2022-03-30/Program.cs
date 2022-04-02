39f8c58bd69e7ba658a6bdb027d682f91d8591c1
Chadolbaegi128 <duswns12893@gmail.com>
2022-04-02 09:53:55 +0900

    Updating Unity/C# script

---
+++ C#/Csharp_2022-03-30/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Csharp_2022_03_30
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[,,] student = new string[2,2,2]
+            {
+                { {"유관순","이화학당" } ,{"홍길동","율촌국"} },
+                 { {"임꺽정","황해도" } ,{"장길산","광대"} } 
+            };
+              
+            foreach(string count in student)
+            {
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
